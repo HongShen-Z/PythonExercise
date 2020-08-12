a5fd1e76845d228c759894cbdcc32a63f6d30fd4
HongShen-Z <62940257+HongShen-Z@users.noreply.github.com>
2020-08-12 16:34:01 +0800

    Update sub_8.py

--- case_00/sub_8.py
+++ case_00/sub_8.py
@@ -3,7 +3,7 @@
 '''
 
 for i in range(1, 10):
-    for j in range(1, i + 1):
-        exp = '%d * %d = %d' % (i, j, i * j)
-        print(exp, end='\t')
-    print()
+    for j in range(1, 10):
+        temp = '{0}*{1}={2}'.format(i, j, i*j)
+        print(temp, end='\t')
+    print('\n')
