4a856152b908862ed588cd757ad336efaae5bea4
HongShen-Z <62940257+HongShen-Z@users.noreply.github.com>
2020-08-13 11:42:37 +0800

    Update sub_14.py

--- case_10/sub_14.py
+++ case_10/sub_14.py
@@ -2,18 +2,16 @@
 题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
 '''
 
-num = int(input('input num: '))
-arr = []  # 质因数列表
+arr = []
+x = int(input('请输入一个正整数：'))
+temp = x
 
-tmp = num
-while tmp != 1:
-    pri = 2
-    while pri != (num + 1):
-        if (tmp % pri) == 0:
-            tmp = tmp // pri
-            arr.append(pri)
-        else:
-            pri += 1
-
-exp = ' * '.join(map(str, arr))
-print('%d = %s' % (num, exp))
+pri = 2
+while pri != (x + 1):
+    if temp % pri == 0:
+        arr.append(pri)
+        temp /= pri
+    else:
+        pri += 1
+exp = '*'.join(map(str, arr))
+print('{}={}'.format(x, exp))
